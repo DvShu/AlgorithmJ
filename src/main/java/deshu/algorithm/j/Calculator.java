72b7173209ef3e4bf0f910a8cc9439827f32888d
haoran.shu <haoran.shu@laifusi.com>
2019-04-17 17:37:28 +0800

    初始提交
    Signed-off-by: haoran.shu <haoran.shu@laifusi.com>

---
+++ src/main/java/deshu/algorithm/j/Calculator.java
@@ -0,0 +1,10 @@
+package deshu.algorithm.j;
+
+/**
+ * Created by haoran.shu on 2019/4/17 17:10.
+ */
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/deshu/algorithmj/test/SortTest.java
@@ -0,0 +1,35 @@
+package deshu.algorithmj.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import deshu.algorithm.j.Calculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * Created by haoran.shu on 2019/4/17 16:49.
+ */
+public class SortTest {
+
+    @Test
+    @DisplayName("1 + 1 = 2")
+    void addsTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+    }
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    void add(int first, int second, int expectedResult) {
+        Calculator calculator = new Calculator();
+        assertEquals(expectedResult, calculator.add(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+}
